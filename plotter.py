0d8148a964ccb83c8b1fc271be6eeba9cdf15eca
William H. Grover <wgrover@engr.ucr.edu>
2018-01-22 20:45:09 -0800

    plotter can now deal with FULL values.

--- plotter.py
+++ plotter.py
@@ -5,14 +5,14 @@ from dateutil import tz
 import matplotlib.dates as mdates
 
 
-data = numpy.loadtxt("outfile.txt", usecols=(2, 3, 4, 5, 6, 7))
+data = numpy.genfromtxt("outfile.txt", usecols=(2, 3, 4, 5, 6, 7), delimiter=" ",
+                        missing_values = "FULL", filling_values = 0,
+                        autostrip = True)
 dates = numpy.loadtxt("outfile.txt", usecols=[0,], dtype="S")
 times = numpy.loadtxt("outfile.txt", usecols=[1,], dtype="S")
 datetimes = [datetime.datetime.strptime(d+t, "%Y-%m-%d%H:%M:%S.%f") for d,t in zip(dates,times)]
-# datetimes = [dt.replace(tzinfo=tz.tzutc()) for dt in datetimes]
-# datetimes = [dt.astimezone(tz.tzlocal()) for dt in datetimes]
 datetimes = matplotlib.dates.date2num(datetimes)
-plt.plot_date(datetimes, data, ".", tz='US/Pacific')
+plt.plot_date(datetimes, data, "-")
 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d/%m/%Y %H:%M'))
 plt.gcf().autofmt_xdate()
 plt.savefig("out.png")
